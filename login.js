f9eae6a14139dbc3e943351fbfc5c4097d57eeb2
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-01-05 21:02:26 +0530

    second commit

---
+++ login.js
@@ -0,0 +1,45 @@
+function check()
+{
+    let tempEmail=document.getElementById('email').value;
+    let temppassword=document.getElementById('password').value;
+    let tempUser;
+    let users=[];
+
+    fetch('https://my-first-page-355f5-default-rtdb.firebaseio.com/user.json').then(response=>response.json()).then(
+        data=>
+        {
+            for(let x in data)
+            {
+                tempUser={
+                    ...data[x]
+                }
+                users.push(tempUser)
+            }
+           
+
+            let test=users.filter((x)=>
+            {
+                return x.email==tempEmail && x.password==temppassword
+            })
+
+            if(test.length==1)
+            {
+                alert('Login successfull');
+                window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+            }
+            
+           let test2 = users.filter((x)=>
+            {
+                return x.email==tempEmail
+            })
+
+            if(test2.length==1 && test.length!=1)
+            {
+                alert('Please enter correct password')
+            }
+            else if(test2.length!=1 && test.length!=1){
+                    alert('No such email exist...Kindly signup')
+                }
+        }
+    )
+}
---
+++ signup.js
@@ -0,0 +1,37 @@
+function check()
+{
+    let username=document.getElementById('name').value;
+    let useremail=document.getElementById('email').value;
+    let pswd1=document.getElementById('password').value;
+    let pswd2=document.getElementById('cpassword').value;
+
+
+    let user={
+        name:username,
+        email:useremail,
+        password:pswd1
+    }
+
+
+    if(pswd1==pswd2 && username.length>1 && useremail.length>3 && pswd1.length>3 && pswd2.length>3)
+    {
+        fetch('https://my-first-page-355f5-default-rtdb.firebaseio.com/user.json',
+        {
+            method:'post',
+            body:JSON.stringify(user)
+        }).then(()=>
+        {
+            alert('Your Data is pushed succesfully into the database.');
+            document.getElementById('name').value="";
+            document.getElementById('email').value="";
+            document.getElementById('password').value="";
+            document.getElementById('cpassword').value="";
+        })      
+    }
+    else if(pswd1!=pswd2){
+        alert('Password and Confirm password should be same.')
+    }
+    else{
+        alert("Please fill all Details.")
+    }
+}
