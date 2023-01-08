86bc6c92b3180c3e02fb181185dfba943d1dbf5e
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-01-08 20:20:10 +0530

    modified

--- signup.js
+++ signup.js
@@ -26,7 +26,7 @@ function check()
             document.getElementById('email').value="";
             document.getElementById('password').value="";
             document.getElementById('cpassword').value="";
-            window.open("./login.html")
+            window.open("./login")
         })      
     }
     else if(pswd1!=pswd2){
